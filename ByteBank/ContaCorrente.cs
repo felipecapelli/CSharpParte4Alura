6b2bca79b981484a409b61cf078c5f100b364bae
felipecapelli <felipe.ccapelli@gmail.com>
2020-09-26 18:43:43 -0300

    Atividades do curso

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,112 @@
+﻿// using _05_ByteBank;
+
+using System;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        private int Agencia { get; }
+
+        private int Numero { get; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0)
+            {
+                ArgumentException excecao = new ArgumentException("Argumento agencia deve ser maior que 0.", nameof(agencia));
+                throw excecao; 
+            }
+
+            if (numero <= 0)
+            {
+                ArgumentException excecao = new ArgumentException("Argumento número deve ser maior que 0.", nameof(numero));
+                throw excecao;
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor invalido para o saque.", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor invalido para a transferência.", nameof(valor));
+            }
+
+
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+
+                throw new OperacaoFinanceiraException("Operação não realizada.", ex);//lanço uma nova exceção, mas as informações da InnerException ficaram encapsuladas dentro do seu objeto
+                //throw; //nesse caso ele vai lançar a stack trace incluindo o metodo Sacar
+                //throw ex; //nesse caso ele vai lançar a stack trace NÃO incluindo o metodo Sacar
+            }
+
+            contaDestino.Depositar(valor);
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch no metodo main");
+            }
+            Console.ReadLine();
+        }
+
+        public static void CarregarContas()
+        {
+
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+            //-----------------------------------------------------
+
+            //LeitorDeArquivo leitor = null;//new LeitorDeArquivo("contas.txt");
+
+            //try
+            //{
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+
+            //    leitor.Fechar();
+            //}
+            //catch (IOException)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada");
+            //}
+            //finally
+            //{
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+
+            //}
+        }
+
+        public static void TestaInnerException()
+        {
+
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(456, 4578420);
+                ContaCorrente conta2 = new ContaCorrente(485, 456478);
+
+                conta2.Transferir(10000000, conta);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(500);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argumento com problemas: " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                //Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
+
+            }
+            catch (OperacaoFinanceiraException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                Console.WriteLine("Message Inner Exception: ----------- " + ex.InnerException.Message);
+                Console.WriteLine("StackTrace Inner Exception: ----------- " + ex.InnerException.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+
+            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+    }
+}
